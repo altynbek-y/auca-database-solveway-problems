6ea1ad13ac0dc977b2e61e67f41693133f086272
Nami Augustine <70685477+altynbek-y@users.noreply.github.com>
2022-01-10 04:29:37 -0500

    Create STF-0006.sql

---
+++ STF-0006.sql
@@ -0,0 +1,81 @@
+select top 1 z.Zodiac,  sum(s.amount)/count(z.id) as AverageSalary
+from
+(select id as id, case 
+	when datepart(month, dateofbirth)=12 and datepart(day, dateofbirth)<22 
+	then 'Sagittarius' 
+
+	when datepart(month, dateofbirth)=12 and datepart(day, dateofbirth)>=22 
+	then 'Capricorn'
+
+	when datepart(month, dateofbirth)=1 and datepart(day, dateofbirth)<20
+	then 'Capricorn' 
+
+	when datepart(month, dateofbirth)=1 and datepart(day, dateofbirth)>=20
+	then 'Aquarius'
+
+	when datepart(month, dateofbirth)=2 and datepart(day, dateofbirth)<19
+	then 'Aquarius' 
+
+	when datepart(month, dateofbirth)=2 and datepart(day, dateofbirth)>=19
+	then 'Pisces'
+
+	when datepart(month, dateofbirth)=3 and datepart(day, dateofbirth)<21
+	then 'Pisces' 
+
+	when datepart(month, dateofbirth)=3 and datepart(day, dateofbirth)>=21
+	then 'Aries'
+
+	when datepart(month, dateofbirth)=4 and datepart(day, dateofbirth)<20
+	then 'Aries' 
+
+	when datepart(month, dateofbirth)=4 and datepart(day, dateofbirth)>=20
+	then 'Taurus'
+
+	when datepart(month, dateofbirth)=5 and datepart(day, dateofbirth)<21 
+	then 'Taurus' 
+
+	when datepart(month, dateofbirth)=5 and datepart(day, dateofbirth)>=21 
+	then 'Gemini'
+
+	when datepart(month, dateofbirth)=6 and datepart(day, dateofbirth)<21
+	then 'Gemini' 
+
+	when datepart(month, dateofbirth)=6 and datepart(day, dateofbirth)>=21 
+	then 'Cancer'
+
+	when datepart(month, dateofbirth)=7 and datepart(day, dateofbirth)<23 
+	then 'Cancer' 
+
+	when datepart(month, dateofbirth)=7 and datepart(day, dateofbirth)>=23 
+	then 'Leo'
+
+	when datepart(month, dateofbirth)=8 and datepart(day, dateofbirth)<23 
+	then 'Leo' 
+
+	when datepart(month, dateofbirth)=8 and datepart(day, dateofbirth)>=23 
+	then 'Virgo'
+
+	when datepart(month, dateofbirth)=9 and datepart(day, dateofbirth)<23
+	then 'Virgo' 
+
+	when datepart(month, dateofbirth)=9 and datepart(day, dateofbirth)>=23 
+	then 'Libra'
+
+	when datepart(month, dateofbirth)=10 and datepart(day, dateofbirth)<24
+	then 'Libra' 
+
+	when datepart(month, dateofbirth)=10 and datepart(day, dateofbirth)>=24 
+	then 'Scorpio'
+
+	when datepart(month, dateofbirth)=11 and datepart(day, dateofbirth)<22 
+	then 'Scorpio' 
+
+	when datepart(month, dateofbirth)=11 and datepart(day, dateofbirth)>=22 
+	then 'Sagittarius'
+end as Zodiac
+from employees
+) as z
+inner join salaries as s
+on z.id = s.employeeid
+group by z.Zodiac
+order by sum(s.amount)/count(z.id)  desc
