fc007cdcfb7c948a380f0c60f1ee8e7d77758994
Nami Augustine <70685477+altynbek-y@users.noreply.github.com>
2022-01-10 04:22:14 -0500

    Create RST-0021.sql

---
+++ RST-0021.sql
@@ -0,0 +1,28 @@
+select top 1 with ties i1.name as Ingredient1, i2.name as Ingredient2
+from(
+  select distinct i1.dishid, i1.ingredientid as Ing1, i2.ingredientid as Ing2
+  from [dbo].[dishesingredients] as i1
+  inner join [dbo].[dishesingredients] as i2
+  on i1.dishid = i2.dishid and i1.ingredientid <> i2.ingredientid
+) a1
+inner join Ingredients as i1
+	on i1.id = Ing1
+inner join Ingredients as i2
+	on i2.id = Ing2
+where 
+  i1.name < i2.name
+group by 
+	Ing1, Ing2, i1.name, i2.name
+having count(*) < (
+select top 1 count(*) as cnt
+from (
+    select distinct i1.dishid, i1.ingredientid as Ing1, i2.ingredientid as Ing2
+    from [dbo].[dishesingredients] as i1
+    inner join [dbo].[dishesingredients] as i2
+    on i1.dishid = i2.dishid and i1.ingredientid <> i2.ingredientid
+    ) a 
+    group by Ing1, Ing2
+    order by cnt desc
+)
+order by 
+	count(*) desc
